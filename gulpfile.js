8859ae27a85e46ae49d08db632ba13b25f2b618e
Scott <hugs@scottriley.im>
2015-01-26 23:38:09 +0000

    Chad’s site stuff innit m8

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var autoprefix = require('gulp-autoprefixer');
+var livereload = require('gulp-livereload');
+
+// CSS concat, auto-prefix and minify
+gulp.task('sass', function() {
+	gulp.src(['./assets/sass/*.scss'])
+		.pipe(sass())
+		.pipe(autoprefix('last 2 versions'))
+		.pipe(gulp.dest('./assets/css/'))
+		.pipe(livereload());
+});
+
+// default gulp task
+gulp.task('default', ['sass'], function() {
+	// watch for CSS changes
+	gulp.watch('./assets/sass/**/*.scss', ['sass']);
+});
